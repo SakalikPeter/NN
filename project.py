5e5aeae81991f8725896bc755f033a45a017e4cc
Peter <peter@pop-os.localdomain>
2021-04-25 11:10:49 +0200

    wanda

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy.random import randn
 from numpy import zeros, ones
 from matplotlib import pyplot
+import wandb
 
 from tensorflow.keras import Sequential
 from tensorflow.keras.optimizers import Adam
@@ -15,22 +16,22 @@ def build_discriminator(in_shape):
 	model = Sequential()
 	# normal
 	model.add(Conv2D(64, (3,3), padding='same', input_shape=in_shape))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# downsample
 	model.add(Conv2D(128, (3,3), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# downsample
 	model.add(Conv2D(128, (3,3), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# downsample
 	model.add(Conv2D(256, (3,3), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# classifier
 	model.add(Flatten())
-	model.add(Dropout(0.4))
+	model.add(Dropout(config['dropout']))
 	model.add(Dense(1, activation='sigmoid'))
 	# compile model
-	opt = Adam(lr=0.0002, beta_1=0.5)
+	opt = Adam(lr=config['d_learning_rate'], beta_1=config['d_beta1'])
 	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 	return model
 
@@ -40,17 +41,17 @@ def build_generator(latent_dim = 100):
 	# foundation for 4x4 image
 	n_nodes = 256 * 4 * 4
 	model.add(Dense(n_nodes, input_dim=latent_dim))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	model.add(Reshape((4, 4, 256)))
 	# upsample to 8x8
 	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# upsample to 16x16
 	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# upsample to 32x32
 	model.add(Conv2DTranspose(128, (4,4), strides=(2,2), padding='same'))
-	model.add(LeakyReLU(alpha=0.2))
+	model.add(LeakyReLU(alpha=config['lrelu']))
 	# output layer
 	model.add(Conv2D(3, (3,3), activation='tanh', padding='same'))
 	return model
@@ -63,7 +64,7 @@ def build_gan(g_model, d_model):
     model.add(g_model)
     model.add(d_model)
 
-    opt = Adam(learning_rate=0.0002, beta_1=0.5)
+    opt = Adam(learning_rate=config['g_learning_rate'], beta_1=config['g_beta1'])
     model.compile(loss='binary_crossentropy', optimizer=opt)
     return model
 
@@ -82,13 +83,13 @@ def generate_real_samples(df, iter, n_samples):
     for _, row in batch.iterrows():
         path = row[0]
         im = Image.open('data/pokemon/'+path)
-        im = im.resize((32, 32))
+        im = im.resize(config['input_shape2'])
         im_array.append(np.array(im))
 
     im_array = np.array(np.float32(im_array))
     im_array = (im_array - 127.5) / 127.5
 
-    y = ones((n_samples, 1))
+    y = ones((n_samples, 1)) - config['smooth']
 
     return im_array, y
 
@@ -109,7 +110,7 @@ def generate_fake_samples(generator, latent_dim, n_samples):
 
 
 # generate samples and save as a plot and save the model
-def summarize_performance(examples, n):
+def summarize_performance(examples, n, iter):
 	# plot images
 	for i in range(n * n):
 		# define subplot
@@ -118,43 +119,101 @@ def summarize_performance(examples, n):
 		pyplot.axis('off')
 		# plot raw pixel data
 		pyplot.imshow(examples[i])
-	pyplot.savefig('output.png')
+	pyplot.savefig(f'output_{iter}.png')
+
+def summarize_accuracy(dataset, n_samples, d_model, g_model):
+	# prepare real samples
+	X_real, y_real = generate_real_samples(dataset, 0, n_samples)
+	# evaluate discriminator on real examples
+	_, acc_real = d_model.evaluate(X_real, y_real, verbose=0)
+	# prepare fake examples
+	x_fake, y_fake = generate_fake_samples(g_model, latent_dim, n_samples)
+	# evaluate discriminator on fake examples
+	_, acc_fake = d_model.evaluate(x_fake, y_fake, verbose=0)
+
+	wandb.log({
+		"Disc real Acc": acc_real,
+		"Disc fake Acc": acc_fake
+	})
 
 
 def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=200, n_batch=128):
-    batch_per_epoch = int(dataset[0].shape[0] / n_batch)
-    half_batch = int(n_batch / 2)
+	batch_per_epoch = int(dataset[0].shape[0] / n_batch)
+	half_batch = int(n_batch / 2)
+	d_loss_real = 0
+	d_loss_fake = 0
+	g_loss = 0
 
-    # manually enumerate epochs
-    for i in range(n_epochs):
-        for j in range(int(batch_per_epoch)):
-            X_real, y_real = generate_real_samples(dataset, j, half_batch)
-            X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+	# manually enumerate epochs
+	for i in range(n_epochs):
+		for j in range(int(batch_per_epoch)):
+			X_real, y_real = generate_real_samples(dataset, j, half_batch)
+			X_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
 
-            d_loss_real = d_model.train_on_batch(X_real, y_real)
-            d_loss_fake = d_model.train_on_batch(X_fake, y_fake)
+			d_loss_real = d_model.train_on_batch(X_real, y_real)
+			d_loss_fake = d_model.train_on_batch(X_fake, y_fake)
 
-            z_input = generate_latent_points(latent_dim, n_batch)
-            y_gan = ones((n_batch, 1))
+			z_input = generate_latent_points(latent_dim, n_batch)
+			y_gan = ones((n_batch, 1))
 
-            g_loss = gan_model.train_on_batch(z_input, y_gan)
+			g_loss = gan_model.train_on_batch(z_input, y_gan)
 
-            print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' % (i+1, j+1, batch_per_epoch, d_loss_real[0], d_loss_fake[0], g_loss))
+			print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' % (i+1, j+1, batch_per_epoch, d_loss_real[0], d_loss_fake[0], g_loss))
 
-        if (i+1) % (1) == 0:
-            latent_points = generate_latent_points(100, 9)
-            X  = g_model.predict(latent_points)
-            X = (X + 1) / 2.0
-            summarize_performance(X, 3)
+		if (i+1) % (5) == 0:
+			latent_points = generate_latent_points(100, 9)
+			X  = g_model.predict(latent_points)
+			X = (X + 1) / 2.0
+			summarize_performance(X, 3, i)
 
+		wandb.log({
+			"Epoch": i,
+			"real Loss": d_loss_real[0],
+			"fake Loss": d_loss_fake[0],
+			"Gen Loss": g_loss
+		})
+
+
+wandb.login()
+
+config = {
+    "epochs": 20,
+    "batch_size": 128,
+	"input_shape1": (32,32,3),
+	"input_shape2": (32,32),
+	"smooth": 0,
+	"lrelu": 0.2,
+	"dropout": 0.4,
+	"batch_norm": False,
+    "loss_function": "binary_crossentropy",
+    "d_optimizer": "Adam",
+	"d_learning_rate": 0.0002,
+	"g_learning_rate": 0.0002,
+	"d_beta1": 0.5,
+	"g_beta1": 0.5,
+	"g_optimizer": "Adam",
+    "dataset": "Pokemon",
+	"comment": ""
+}
+
+run = wandb.init(project='nsfitt-pa', entity='nsfitt-pa')
+wandb.config.update(config)
 
 latent_dim = 100
 df = load_real_data()
 
 g_model = build_generator()
 print(g_model.summary())
-d_model = build_discriminator((32,32,3))
+d_model = build_discriminator(config['input_shape1'])
 print(d_model.summary())
 gan_model = build_gan(g_model, d_model)
 
-train(g_model, d_model, gan_model, df, latent_dim)
+train(g_model, d_model, gan_model, df, latent_dim, n_epochs=config['epochs'], n_batch=config['batch_size'])
+
+summarize_accuracy(df, config['batch_size'], d_model, g_model)
+
+latent_points = generate_latent_points(100, 1)
+X  = g_model.predict(latent_points)
+X = (X + 1) / 2.0
+wandb.log({"examples": [wandb.Image(X, caption="Output")]})
+run.finish()
